54f2c095738d6cb1db926521f39f3718022678a6
TwinG <koboyotml2@gmail.com>
2026-06-29 09:13:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
   base: '/MomyBirthday/',
   plugins: [
     react(),
     tailwindcss(),
-    viteSingleFile(),
   ],
 })
